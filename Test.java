2bb9a8d6fa1d284b6b32deb51cdcc01ff7cf375d
WaterArson <jtghent@mtu.edu>
2026-02-11 11:36:12 -0500

    Update Test.java
    vopid

--- Test.java
+++ Test.java
@@ -1,5 +1,5 @@
 public class Test {
-  public static vopid main(String[] args) {
+  public static void main(String[] args) {
     System.out.println("welcome");
     System.out.println("welcome2: electric boogaloo");
   }
