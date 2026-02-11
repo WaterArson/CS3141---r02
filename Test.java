12afadd5f7157c5156c5911e419fd53df5ddc62c
WaterArson <jtghent@mtu.edu>
2026-02-11 11:32:48 -0500

    Update Test.java
    updated test file

--- Test.java
+++ Test.java
@@ -1,5 +1,6 @@
 public class Test {
   public static vopid main(String[] args) {
-    System.out.println("welcome")
+    System.out.println("welcome");
+    System.out.println("welcome2: electric boogaloo");
   }
 }
