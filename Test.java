6eb7f0f9c7a2882101237ea2e61144c48904a70d
WaterArson <jtghent@mtu.edu>
2026-02-18 19:54:18 -0500

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,5 +1,10 @@
 public class Test {
   public static vopid main(String[] args) {
+<<<<<<< HEAD
     System.out.println("welcome")
+=======
+    System.out.println("welcome");
+    System.out.println("welcome2: electric boogaloo");
+>>>>>>> parent of 2bb9a8d (Update Test.java)
   }
 }
